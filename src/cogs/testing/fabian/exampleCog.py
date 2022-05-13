1c8bfd557f47d227ff469812697ec88be2efa524
xfazze <1339139+XFazze@users.noreply.github.com>
2022-05-13 22:54:11 +0200

    changed discord.Bot to commands.Bot

--- src/cogs/testing/fabian/exampleCog.py
+++ src/cogs/testing/fabian/exampleCog.py
@@ -1,13 +1,17 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, bridge
 
-class exampleCog(commands.Cog): 
+
+class exampleCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
+    @bridge.bridge_command()
+    async def hello(self, ctx, a: discord.Option(int)):
+        sum = a
+        await ctx.respond(f"newest, this is a discord comm{sum}and from a cog!")
 
 
-
-def setup(bot): # this is called by Pycord to setup the cog
-    bot.add_cog(exampleCog(bot)) # add the cog to the bot
+def setup(bot):  # this is called by Pycord to setup the cog
+    bot.add_cog(exampleCog(bot))  # add the cog to the bot
--- src/createBot.py
+++ src/createBot.py
@@ -7,12 +7,10 @@ cogs = [
 
 
 def createBot():
-    bot = discord.Bot(debug_guilds=[802298523214938153], help_command=commands.MinimalHelpCommand()) 
+    bot = commands.Bot(
+        debug_guilds=[802298523214938153], help_command=commands.MinimalHelpCommand())
 
     for cog in cogs:
         bot.load_extension(f'src.cogs.{cog}')
 
-    @bot.command(description="Sends the bot's latency.") 
-    async def ping(ctx): 
-        await ctx.respond(f"Pong! Latency is {bot.latency}")
     return bot
