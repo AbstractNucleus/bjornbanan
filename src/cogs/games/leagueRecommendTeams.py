9bcda401fe81a76792dc0e065feadcbd792a187a
xfazze <1339139+XFazze@users.noreply.github.com>
2022-05-15 00:01:58 +0200

    started working on leagueRecommendTeams

--- src/cogs/games/leagueRecommendTeams.py
+++ src/cogs/games/leagueRecommendTeams.py
@@ -5,6 +5,37 @@ from discord.commands import SlashCommandGroup
 # utdata: list of 2 teams, teams have list of discord ids [[id...],[id...]]
 
 
+class removePlayer(discord.ui.View):
+    @discord.ui.select(
+        placeholder="Choose a player to remove",
+        min_values=1,
+        max_values=3,
+        options=[
+            discord.SelectOption(
+                label="online",
+                value='0'
+            ),
+            discord.SelectOption(
+                label="offline",
+                value='1'
+            ),
+            discord.SelectOption(
+                label="idle",
+                value='2'
+            ),
+            discord.SelectOption(
+                label="dnd",
+                value='3'
+            )
+        ]
+    )
+    async def select_callback(self, select, interaction):
+        statuses = [discord.Status.online, discord.Status.offline,
+                    discord.Status.idle, discord.Status.dnd, discord.Status.streaming]
+        await interaction.client.change_presence(status=statuses[int(select.values[0])])
+        await interaction.message.edit_message(embed=discord.Embed(title=f"Changed status to {statuses[int(select.values[0])]}", color=0x00FF42))
+
+
 class leagueRecommendTeams(commands.Cog):
 
     def __init__(self, bot):
@@ -24,6 +55,8 @@ class leagueRecommendTeams(commands.Cog):
             if user.bot:
                 continue
             users.append(user)
+
+        # select UI to delete poeple
 
         if len(list(ctx.author.voice.channel.members)) > 10:
             await ctx.respond(embed=discord.Embed(title="You need to be in a voice channel", color=0xFD3333))
